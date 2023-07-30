b55e2f29ee23f2205ab39afd95990ab54fe9b1d5
Sourcery AI <>
2023-07-30 22:01:43 +0000

    'Refactored by Sourcery'

--- source-contrib/BibleVerses.popclipext/grabverses.py
+++ source-contrib/BibleVerses.popclipext/grabverses.py
@@ -8,21 +8,18 @@ version = os.environ['POPCLIP_OPTION_VERSION']
 text = unicode(input, "utf-8")
 pattern = "(\\d+\\s)?(\\w+?\\.?\\s\\d+:\\d+)(.\\d+)?(\\d+(.\\d+)?)*"
 verses = re.findall(pattern, input)
-verseList = []
-for item in verses:
-    verseList.append(''.join(map(str, item)))
+verseList = [''.join(map(str, item)) for item in verses]
 
 def chunks(list, listSize):
     return [list[i:i + listSize] for i in range(0, len(list), listSize)]
 
 verseChunks = chunks(verseList, 25)
 
-verseListsToUrls = []
-for item in verseChunks:
-    verseListsToUrls.append(', '.join(map(str, item)))
-
+verseListsToUrls = [', '.join(map(str, item)) for item in verseChunks]
 if not verseListsToUrls:
 	sys.exit(1)
 
 for item in verseListsToUrls:
-    webbrowser.open("http://www.biblegateway.com/passage/?search=" + item + "&version=" + version + "&interface=print")
+    webbrowser.open(
+        f"http://www.biblegateway.com/passage/?search={item}&version={version}&interface=print"
+    )
